f502c2220b85ce1824a0fd833e3ffec54cf8cf79
Eric John Juta <ericjohnjuta@gmail.com>
2015-06-10 14:30:06 +0100

    Added register view page

---
+++ src/cljs/soulection/components/register/view.cljs
@@ -0,0 +1,9 @@
+(ns soulection.components.register.view
+  (:require [re-frame.core :refer [dispatch subscribe dispatch-sync]]))
+
+(defn component []
+  (let [x 1]
+    (fn []
+      [:div.register
+       [:h1.white "Register-page"]
+       [:a {:href "#/home"} "Click me to render the homepage!"]])))
